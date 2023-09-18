e68ff9b6648e1e60025c87c5877c12843cbb8186
iwbc <1209760+iwbc@users.noreply.github.com>
2023-09-19 06:09:51 +0900

    remove jest-junit

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,5 @@
 module.exports = {
   testEnvironment: 'jest-environment-node-single-context',
   testMatch: ['**/__tests__/**/*.{js,jsx,cjs,mjs,ts,tsx}', '**/?(*.)+(spec|test).{js,jsx,cjs,mjs,ts,tsx}'],
-  moduleFileExtensions: ['js', 'cjs', 'scss'],
-  reporters: ['default', 'jest-junit'],
+  moduleFileExtensions: ['js', 'cjs', 'scss']
 };
