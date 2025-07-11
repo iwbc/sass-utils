8edf2dbb6fcffa6c4ccf6edb760fc7eba9e73ca1
iwbc <1209760+iwbc@users.noreply.github.com>
2025-07-11 18:02:52 +0900

    update deps

--- jest.config.cjs
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  testEnvironment: 'jest-environment-node-single-context',
-  testMatch: ['**/__tests__/**/*.{js,jsx,cjs,mjs,ts,tsx}', '**/?(*.)+(spec|test).{js,jsx,cjs,mjs,ts,tsx}'],
-  moduleFileExtensions: ['js', 'cjs', 'scss']
-};
